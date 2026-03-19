ed110deb048db6673a925dd22d20148790307322
Ivan <boguslavets.ivan@lll.kpi.ua>
2026-03-19 18:51:47 +0200

    zvit

--- base-path.js
+++ base-path.js
@@ -1,3 +1,5 @@
 export const BASE_PATH =
   process.env.BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? "/IO-35_appRECORD-Bohuslavets-Ivan-FIOT-2026" : "");
+  (process.env.NODE_ENV === "production"
+    ? "/IO-35_appRECORD-Bohuslavets-Ivan-FIOT-2026"
+    : "");
